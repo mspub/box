4f148dc65656d423262a90fbedbaa6bd282451db
ok <aaa@daum.net>
2016-07-13 21:17:13 +0900

    iris  구조확인하기
    +head(iris)
    +iris[1:6,]
    +iris[1:5,"Sepal.Length"]
    +iris$Sepal.Length[1:5]
    +summary(iris)
    +data(iris)
    +iris
    +var(iris$Sepal.Length)

---
+++ no_1.R
@@ -0,0 +1,60 @@
+?install.packages
+
+
+#commnet
+old.packages()
+head(iris,n=10)
+summary(iris)
+var(iris$Sepal.Length)
+(v<-c(1,3,4,6))
+a<-v[c(1,2,3)]
+a
+v
+v[v>2]
+v>2
+v[-1]
+v[-3]
+v[-lenghth(v)]
+v[-length(v)]
+
+data=read.table(header=T, text='
+                subject sex size
+                1 M 7
+                2 F 6
+                3 M 11
+                ')
+data
+data[1,3]
+data
+data[1:2,]
+data[data$subject <3]
+(b<-4)
+
+v
+subset(data, subject<3, select=-subject)
+data
+subset(data, subject<3 & sex=="M")
+
+
+install.packages("ggplot2")
+library(ggplot2)
+search()
+data()
+data(iris)
+dim(iris)
+class(iris)
+names(iris)
+length(iris)
+colnames(iris)
+names(iris)
+str(iris)
+attributes(iris)
+
+head(iris)
+iris[1:6,]
+iris[1:5,"Sepal.Length"]
+iris$Sepal.Length[1:5]
+summary(iris)
+data(iris)
+iris
+var(iris$Sepal.Length)
